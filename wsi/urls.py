a8a6cdff372b766b427a448aaf07dca5f0ce29ba
Ola Tarkowska <ola@entrypoint.tech>
2020-01-31 10:38:53 +0000

    refactor url patterns

--- wsi/urls.py
+++ wsi/urls.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
 from . import views
 
 
-urlpatterns = patterns(
-    'django.views.generic.simple',
-
+urlpatterns = [
     url(r'^login/$', views.CustomWebclientLoginView.as_view(), name="wsi_weblogin"),
-
-)
+]
